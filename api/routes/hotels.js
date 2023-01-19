bb80a541d4901988f6f9a88b855a20adad54aa55
temple_19 <tozoaz@gmail.com>
2023-01-19 19:46:14 +0100

    2nd commit

---
+++ api/routes/hotels.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  updateHotel,
+  getHotels,
+} from '../controllers/hotel.js';
+import Hotel from '../models/Hotel.js';
+
+const router = express.Router();
+
+//create
+router.post('/', createHotel);
+//update
+router.put('/:id', updateHotel);
+//delete
+router.delete('/:id', deleteHotel);
+//get
+router.get('/:id', getHotel);
+//getall
+router.get('/', getHotels);
+export default router;
